19eb6ef4d678d0122739e24b45a61b40c9ac45cf
Johannes Zellner <johannes@nebulon.de>
2016-02-15 21:17:44 +0100

    Fix mobile layout

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ var gulp = require('gulp'),
     run = require('gulp-run'),
     gutil = require('gulp-util');
 
-gulp.task('styles', function () {
+gulp.task('styles', [ 'styles-material' ], function () {
     return gulp.src('frontend/scss/index.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({ includePaths: [] }).on('error', sass.logError))
@@ -21,6 +21,17 @@ gulp.task('styles', function () {
         .pipe(minifycss())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('public/'));
+});
+
+gulp.task('styles-material', function () {
+    return gulp.src('frontend/3rdparty/materialize-src/sass/materialize.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({ includePaths: [] }).on('error', sass.logError))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('public/3rdparty/'));
 });
 
 gulp.task('browserify', function () {
