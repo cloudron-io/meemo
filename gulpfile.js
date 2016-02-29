ee33b97436a706380b0d1feee5d742b7c5186c3d
Johannes Zellner <johannes@nebulon.de>
2016-02-29 14:04:53 +0100

    Remove materialize-css copy

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ var gulp = require('gulp'),
     buffer = require('vinyl-buffer'),
     run = require('gulp-run');
 
-gulp.task('styles', [ 'styles-material' ], function () {
+gulp.task('styles', function () {
     return gulp.src('frontend/scss/index.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({ includePaths: [] }).on('error', sass.logError))
@@ -21,17 +21,6 @@ gulp.task('styles', [ 'styles-material' ], function () {
         .pipe(minifycss())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('public/'));
-});
-
-gulp.task('styles-material', function () {
-    return gulp.src('frontend/3rdparty/materialize-src/sass/materialize.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({ includePaths: [] }).on('error', sass.logError))
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(minifycss())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('public/3rdparty/'));
 });
 
 gulp.task('browserify', function () {
