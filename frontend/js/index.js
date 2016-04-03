86cd0add68d0599d112b09cae0911429d0485fcd
Johannes Zellner <johannes@nebulon.de>
2016-04-03 12:44:42 +0200

    No need for separate filter.js

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -5,6 +5,44 @@ Vue.config.debug = true;
 window.Guacamoly = window.Guacamoly || {};
 var Core = window.Guacamoly.Core;
 
+var md = window.markdownit({
+    breaks: true,
+    html: true,
+    linkify: true
+}).use(window.markdownitEmoji)
+.use(window.markdownitCheckbox);
+
+md.renderer.rules.emoji = function(token, idx) {
+  return twemoji.parse(token[idx].content);
+};
+
+Vue.filter('markdown', function (value) {
+    if (!value) return '';
+
+    return md.render(value);
+});
+
+Vue.filter('prettyDateOffset', function (time) {
+    var date = new Date(time),
+        diff = (((new Date()).getTime() - date.getTime()) / 1000),
+        day_diff = Math.floor(diff / 86400);
+
+    if (isNaN(day_diff) || day_diff < 0)
+        return;
+
+    return day_diff === 0 && (
+            diff < 60 && 'just now' ||
+            diff < 120 && '1 minute ago' ||
+            diff < 3600 && Math.floor( diff / 60 ) + ' minutes ago' ||
+            diff < 7200 && '1 hour ago' ||
+            diff < 86400 && Math.floor( diff / 3600 ) + ' hours ago') ||
+        day_diff === 1 && 'Yesterday' ||
+        day_diff < 7 && day_diff + ' days ago' ||
+        day_diff < 31 && Math.ceil( day_diff / 7 ) + ' weeks ago' ||
+        day_diff < 365 && Math.round( day_diff / 30 ) +  ' months ago' ||
+                          Math.round( day_diff / 365 ) + ' years ago';
+});
+
 function getCurrentSearchWord() {
     if (!vue) return '';
 
