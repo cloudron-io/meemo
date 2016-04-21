29659a7ad5784e4ce09d64e7f9e509c35e3f1115
Johannes Zellner <johannes@nebulon.de>
2016-04-21 16:08:32 +0200

    Change default title

--- src/settings.js
+++ src/settings.js
@@ -36,7 +36,7 @@ function get(callback) {
     g_settings.find({ type: 'frontend' }).toArray(function (error, result) {
         if (error) return callback(error);
         callback(null, result[0] ? result[0].value : {
-            title: 'Things'
+            title: 'Guacamoly'
         });
     });
 }
