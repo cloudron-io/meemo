a22f5ed22baa555fb48d8c1aa85c8d0d6f1b821b
Johannes Zellner <johannes@nebulon.de>
2016-12-01 14:44:51 +0100

    Remove now invalid comment

--- app.js
+++ app.js
@@ -38,9 +38,7 @@ router.put ('/api/things/:id', routes.auth, routes.put);
 router.del ('/api/things/:id', routes.auth, routes.del);
 
 router.post('/api/files', routes.auth, memoryUpload, routes.fileAdd);
-// FIXME should not be public!!! but is required for thing sharing at the moment
 router.get ('/api/files/:userId/:identifier', routes.fileGet);
-
 
 router.get ('/api/tags', routes.auth, routes.getTags);
 
