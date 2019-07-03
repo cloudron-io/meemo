654661d8274a89a63ef808b7b84a7e04d3175014
Girish Ramakrishnan <girish@cloudron.io>
2019-07-03 07:19:21 -0700

    Fix display of mailbox name

--- src/routes.js
+++ src/routes.js
@@ -137,7 +137,7 @@ function profile(req, res, next) {
         if (error) return next(new HttpError(500, error));
 
         var out = {
-            mailbox: process.env.CLOUDRON_MAIL_TO ? (process.env.CLOUDRON_MAIL_IMAP_USERNAME + '+' + result.username + '@' + process.env.CLOUDRON_MAIL_DOMAIN) : null,
+            mailbox: process.env.CLOUDRON_MAIL_TO ? (process.env.CLOUDRON_MAIL_IMAP_USERNAME.split('@')[0] + '+' + result.username + '@' + process.env.CLOUDRON_MAIL_DOMAIN) : null,
             user: result
         };
 
