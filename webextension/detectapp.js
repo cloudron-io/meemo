bcf48ec48d770d35527adf65db0919e9e009138d
Johannes Zellner <johannes@nebulon.de>
2019-11-12 19:13:05 +0100

    Some extension cleanups and add selected text by default
    Fixes #87

--- webextension/detectapp.js
+++ webextension/detectapp.js
@@ -2,9 +2,13 @@
 
 /* global chrome */
 
+// swap the boolean to show debug info
+var debug = false ? console.log : function () {};
+
 var settingsNode = document.getElementById('guacamoly-settings-node');
 if (settingsNode) {
     try {
+        debug(settingsNode.textContent);
         chrome.runtime.sendMessage(JSON.parse(settingsNode.textContent));
     } catch (e) {}
 }
--- webextension/popup.js
+++ webextension/popup.js
@@ -2,58 +2,15 @@
 
 /* global chrome */
 
+// Here we use `chrome` instead of `browser` to stay compatible. See https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Chrome_incompatibilities
+
+// swap the boolean to show debug info
+var debug = false ? console.log : function () {};
+
 var queryInfo = {
     active: true,
     currentWindow: true
 };
-
-console.log('----')
-window.resizeTo(400, 500);
-console.log(window.document)
-
-chrome.runtime.onMessage.addListener(function (msg, sender) {
-    if (msg.origin && msg.token && msg.title) {
-        localStorage.origin = msg.origin;
-        localStorage.token = msg.token;
-        localStorage.title = msg.title;
-        show('home');
-    }
-});
-
-function getCurrentTabUrl(callback) {
-    chrome.tabs.query(queryInfo, function(tabs) {
-        callback(tabs[0].url);
-    });
-}
-
-function tryToDetectSettings() {
-    chrome.tabs.query(queryInfo, function(tabs) {
-        chrome.tabs.executeScript(tabs[0].id, { file: 'detectapp.js' });
-    });
-}
-
-tryToDetectSettings();
-
-document.addEventListener('DOMContentLoaded', function() {
-    if (!localStorage.token) {
-        show('setup');
-    } else {
-        show('main');
-
-        document.getElementById('actionAdd').addEventListener('click', add);
-
-        getCurrentTabUrl(function(url) {
-            document.getElementById('content').value = url + '\n\n';
-        });
-
-        // setting autofocus or set focus immediately wont work
-        setTimeout(function () { document.getElementById('content').focus(); }, 250);
-
-        // Register event handlers
-        shortcut.add('Ctrl+s', add, {});
-        shortcut.add('Ctrl+Enter', add, {});
-    }
-});
 
 var views = [
     'main',
@@ -70,6 +27,56 @@ function show(view) {
 
     document.getElementById(view).style.display = 'block';
 }
+
+chrome.runtime.onMessage.addListener(function (msg, sender) {
+    if (msg.origin && msg.token && msg.title) {
+        localStorage.origin = msg.origin;
+        localStorage.token = msg.token;
+        localStorage.title = msg.title;
+        show('home');
+    }
+});
+
+function tryToDetectSettings() {
+    chrome.tabs.query(queryInfo, function(tabs) {
+        chrome.tabs.executeScript(tabs[0].id, { file: 'detectapp.js' }, function () {
+            if (chrome.runtime.lastError) debug('Failed to execute detectapp.js', chrome.runtime.lastError);
+        });
+    });
+}
+
+tryToDetectSettings();
+
+document.addEventListener('DOMContentLoaded', function() {
+    if (!localStorage.token) {
+        show('setup');
+    } else {
+        show('main');
+
+        document.getElementById('actionAdd').addEventListener('click', add);
+
+        chrome.tabs.query(queryInfo, function(tabs) {
+            var content = '';
+
+            // try to get selected text
+            chrome.tabs.executeScript(tabs[0].id, { code: 'window.getSelection().toString();' }, function (selection) {
+                if (chrome.runtime.lastError) debug('Failed to execute detectapp.js', chrome.runtime.lastError);
+
+                if (selection && selection[0]) content += selection[0] + '\n\n';
+                content += tabs[0].url + '\n\n';
+
+                document.getElementById('content').value = content;
+            });
+        });
+
+        // setting autofocus or set focus immediately wont work
+        setTimeout(function () { document.getElementById('content').focus(); }, 250);
+
+        // Register event handlers
+        shortcut.add('Ctrl+s', add, {});
+        shortcut.add('Ctrl+Enter', add, {});
+    }
+});
 
 function add() {
     show('busy');
